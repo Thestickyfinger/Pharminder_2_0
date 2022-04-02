c72dc2c25b06f202f81bf843bae71e62d475af89
Daniel Berjon <danielberjon@solvium.es>
2022-04-02 12:44:32 +0200

    Calendario

--- app/src/main/java/com/example/pharminder_2_0/CalendarAdapter.java
+++ app/src/main/java/com/example/pharminder_2_0/CalendarAdapter.java
@@ -1,24 +1,53 @@
 package com.example.pharminder_2_0;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.view.View;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 
-public class CalendarAdapter extends RecyclerView.Adapter {
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder> {
+
+    private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener;
+
+    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener) {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.calendar_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalendarViewHolder(view, onItemListener);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+
+        holder.dayOfMonth.setText(daysOfMonth.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return daysOfMonth.size();
     }
+
+    public interface OnItemListener{
+
+        void onItemClick(int position, String daytex);
+
+    }
+
 }
--- app/src/main/java/com/example/pharminder_2_0/CalendarViewHolder.java
+++ app/src/main/java/com/example/pharminder_2_0/CalendarViewHolder.java
@@ -6,12 +6,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder {
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-    private final TextView dayOfmonth;
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
 
-    public CalendarViewHolder(@NonNull View itemView) {
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener) {
         super(itemView);
-        dayOfmonth = itemView.findViewById(R.id.cellDayText);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
     }
 }
